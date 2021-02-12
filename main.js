0dd07862e4a835f92abb105be7ced5563983acfa
Gladyshev445 <74065918+Gladyshev445@users.noreply.github.com>
2021-02-12 19:43:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let x = 20
+    y = 10
+    c = "computer"
+    //console.log(x**y)
+    //console.log(x+y)
+    //console.log(x-y)
+    //console.log(x%y)
+    //console.log(x*y)
+    //console.log(x = x+5)
+    //console.log(x += 5)
+    //console.log(y = y/7)
+
+    console.log(x)
+    console.log(x==y)
+    console.log(x!=y)
+    console.log(x > y)
+    console.log(x => y)
+    console.log(c < x+y)
+    console.log(c)
